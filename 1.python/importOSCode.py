3cb75ced4c77c86fdefa54eddd7a97922064a783
Srushti <rindhesrushti2006@gmail.com>
2025-12-27 13:57:47 +0530

    import os code o print the list of directories present in my c drive

---
+++ 1.python/importOSCode.py
@@ -0,0 +1,12 @@
+import os
+
+# Specify the directory path
+directory_path = "/"  # change this to the path you want
+
+# Get the list of files and directories
+contents = os.listdir(directory_path)
+
+# Print each item
+print(f"Contents of '{directory_path}':")
+for item in contents:
+    print(item)
