8077a02922f1feb8cb53cf1b5be6a794039cbcb0
Srushti <rindhesrushti2006@gmail.com>
2025-12-27 19:30:33 +0530

    added output in files

--- 1.python/importOSCode.py
+++ 1.python/importOSCode.py
@@ -8,3 +8,9 @@ contents = os.listdir(directory_path)
 
 # Print each item
 print(contents)
+
+'''
+Output
+listed folder from my c drive 
+['$Recycle.Bin', 'Documents and Settings', 'DumpStack.log', 'DumpStack.log.tmp', 'hiberfil.sys', 'inetpub', 'Intel', 'MSOCache', 'pagefile.sys', 'PerfLogs', 'Program Files', 'Program Files (x86)', 'ProgramData', 'Recovery', 'swapfile.sys', 'System Volume Information', 'Users', 'Windows']
+'''
