c5b3f672b80e73d922f4edc648de210a7e6eee2e
Srushti <rindhesrushti2006@gmail.com>
2025-12-27 19:41:27 +0530

    solved the problem of area and perimeter of square

---
+++ 1.python/area&perimeterOfSquare.py
@@ -0,0 +1,18 @@
+'''
+Area and Perimeter of Square
+Write a program to find the area and perimeter of square whose side length is 4.5. 
+On the first line, print square's area and on the second line, its perimeter.
+Note:
+Area of a Square = side × side
+and, Perimeter = 4 x side
+'''
+
+side = 4.5
+area = side * side
+perimeter = 4 * side
+print(area)
+print(perimeter)
+
+# Output -
+# 20.25
+# 18.0
