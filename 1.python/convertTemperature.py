ed507fce9e2182d0ca244be8e37f634556ae5195
Srushti <rindhesrushti2006@gmail.com>
2025-12-27 19:35:58 +0530

    solved temperature convert problem

---
+++ 1.python/convertTemperature.py
@@ -0,0 +1,20 @@
+'''
+Convert Temperature
+Declare a variable "temperature" and initialize it with a value of 25.5 (in Celsius) and print 
+it in Celsius and Kelvin (add 273 to the temperature in Celsius).
+
+[Note: Print the output in exactly the same format as given below. There is single space around 
+hyphen(-)]
+
+Output Format
+Celsius - 25.5
+Kelvin - 298.5
+'''
+temperature = 25.5
+
+print("Celsius -", temperature)
+print("Kelvin -",25.5 + 273)
+
+# Output -
+# Celsius - 25.5
+# Kelvin - 298.5
