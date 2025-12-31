d50f223a87cf0920a66da3825790cf9f4034a16f
Srushti <rindhesrushti2006@gmail.com>
2025-12-31 14:23:38 +0530

    leanr about logical operator in python

---
+++ 1.python/logicalOperator.py
@@ -0,0 +1,15 @@
+# OR operator
+print("true or true", True or True)
+print("true or false", True or False)
+print("false or true", False or True)
+print("flase or false", False or False)
+
+# AND operator
+print("true and true", True and True)
+print("true and false", True and False)
+print("false and true", False and True)
+print("flase and false", False and False)
+
+#NOT operator
+print(not(True))
+print(not(False))
