91d267c6fedda54a32d4788e46e221d99294c476
Srushti <rindhesrushti2006@gmail.com>
2026-01-01 19:24:18 +0530

    solved problem of finding average of two number

---
+++ 1.python/avgOftwoNos.py
@@ -0,0 +1,10 @@
+# Write a python program to find an average of two numbers entered by the user. 
+
+a = int(input("Enter the first number"))
+b = int(input("Enter the second Number"))
+avg = ((a+b)/2)
+print("Average of two number is", avg)
+
+#Enter the first number: 12
+# Enter the second Number: 56
+# Average of two number is 34.0
