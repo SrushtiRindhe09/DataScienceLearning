b6362e9d6088c4d5a11e7406beeebd8bbea2c7fb
Srushti <rindhesrushti2006@gmail.com>
2026-01-01 21:36:52 +0530

    started solving string related problems

---
+++ 1.python/String/displayuserEnteredName.py
@@ -0,0 +1,11 @@
+'''
+Write a python program to display a user entered name followed by Good 
+Afternoon using input () function. 
+'''
+
+name = input("enter the name of user: ")
+print(f"Good Morning {name}") #fstring
+
+# output
+# enter the name of user: srushti
+# Good Morning srushti
