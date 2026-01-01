a90c8021e8705ade30b1f00f336be63cb7c8515c
Srushti <rindhesrushti2006@gmail.com>
2026-01-01 19:09:48 +0530

    solved some basic python problem

---
+++ 1.python/checkTypeOfVariable.py
@@ -0,0 +1,10 @@
+# Check the type of variable assigned using input () function. 
+a = input("enter the number")
+t = type(a)
+print(t) 
+
+'''
+Output:
+enter the number: 12
+<class 'str'>
+'''
---
+++ 1.python/findRemainder.py
@@ -0,0 +1,7 @@
+num = 7
+mod = num % 2
+print (mod) #1
+
+a = 34
+b = 5
+print("remainder when a is dvided by b",a % b) # 4
