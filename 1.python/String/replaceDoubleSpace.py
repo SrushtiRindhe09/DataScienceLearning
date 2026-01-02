3a64197ee5979cf64d1bad324ea56dbe57c29a4f
Srushti <rindhesrushti2006@gmail.com>
2026-01-02 19:09:04 +0530

    learn to use string function

---
+++ 1.python/String/replaceDoubleSpace.py
@@ -0,0 +1,9 @@
+# Replace the double space from problem 3 with single spaces. 
+
+value = "hello srushti   how are you!"
+print(value.replace("  ", " "))
+print(value) # string is immutable, we cannot change original string.
+
+# output
+# hello srushti how are you!
+# hello srushti   how are you!
