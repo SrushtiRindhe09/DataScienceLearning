e6b431b149268388e3af7221a181822ade45cada
Srushti <rindhesrushti2006@gmail.com>
2026-01-02 20:02:21 +0530

    learn to use string function

---
+++ 1.python/String/stringFunctions.py
@@ -0,0 +1,19 @@
+str = "srushti"
+print(str) #return a string
+print(str.find("s")) #find the index of character in string.
+print(str.replace("r","e")) # replace character 
+print(str.startswith("s")) # return boolen value 
+print(str.endswith("e"))
+print(str.count("s")) # count character repetation in string
+print(len(str)) # return lenghth of string start from 1
+print(str)
+
+# srushti
+# 0
+# seushti
+# True
+# False
+# 2
+# 7
+# Srushti
+# srushti
