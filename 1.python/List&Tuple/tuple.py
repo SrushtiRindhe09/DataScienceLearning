9242ecc48e425b4cdf29965684528b3a3609620a
Srushti <rindhesrushti2006@gmail.com>
2026-01-03 20:56:57 +0530

    changed the folder name

---
+++ 1.python/List&Tuple/tuple.py
@@ -0,0 +1,6 @@
+a = () # empty tuple
+b = (1,) # tuple with only one element needs a comma
+t = (1,6,3,9,5) # tuple with multiple elements 
+#tuple methods
+print(t.count(9)) # return the number how many times it appear in tuple 
+print(t.index(6)) # return the index of 6 in tuple
