82db9e138b344f063aa01ea34b886092a11bfea4
Srushti <rindhesrushti2006@gmail.com>
2026-01-03 21:21:23 +0530

    solved question 1st lists related

---
+++ 1.python/List&Tuple/listOfFruits.py
@@ -0,0 +1,28 @@
+#Write a program to store seven fruits in a list entered by the user. 
+
+fruits = []
+f1 = input("Enter the fruit name :")
+fruits.append(f1)
+f2 = input("Enter the fruit name :")
+fruits.append(f2)
+f3 = input("Enter the fruit name :")
+fruits.append(f3)
+f4 = input("Enter the fruit name :")
+fruits.append(f4)
+f5 = input("Enter the fruit name :")
+fruits.append(f5)
+f6 = input("Enter the fruit name :")
+fruits.append(f6)
+f7 = input("Enter the fruit name :")
+fruits.append(f7)
+print(fruits)
+
+# output
+# Enter the fruit name :apple
+# Enter the fruit name :mango
+# Enter the fruit name :guava
+# Enter the fruit name :strawberry
+# Enter the fruit name :raspberry
+# Enter the fruit name :jackfruit
+# Enter the fruit name :banana
+# ['apple', 'mango', 'guava', 'strawberry', 'raspberry', 'jackfruit', 'banana']
