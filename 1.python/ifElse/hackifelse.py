c094c4126dda5a8b63e6454fe95f0003f6dd205b
Srushti <rindhesrushti2006@gmail.com>
2026-01-03 21:30:21 +0530

    solved question 1st lists related

---
+++ 1.python/ifElse/hackifelse.py
@@ -0,0 +1,20 @@
+'''
+Task
+Given an integer, n, perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of 2 to 5, print Not Weird
+If  is even and in the inclusive range of 6 to 12, print Weird
+If  is even and greater than 20, print Not Weird
+'''
+n = int(input().strip())
+
+if n % 2 != 0:
+    print("Weird")
+else:
+    if 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
